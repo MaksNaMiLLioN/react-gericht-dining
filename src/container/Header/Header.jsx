bbc708a48e252370c0603b1ca5a576ab9634ad43
Nuraly Tyulkubayev <tsnurali@mail.ru>
2022-02-01 17:03:34 +0600

    Complete Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import images from "../../constants/images";
+import "./Header.css";
+import SubHeading from "../../components/SubHeading/SubHeading";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        incidunt culpa corrupti perferendis.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
