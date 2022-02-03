b4d5b75d321889539622d94f2f3da9f7c8880cc8
Nuraly Tyulkubayev <tsnurali@mail.ru>
2022-02-03 21:50:41 +0600

    Complete Laurels, Intro & Gallery

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
-    <div className="app__aboutus-overlay flex__center ">
+    <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
 
